9b8ec8501f772ae9103a6c4e4147cff4e8f54dcb
saranshgupta2711 <160781953+saranshgupta2711@users.noreply.github.com>
2024-03-26 21:58:21 +0530

    Update fund.sol

--- fund.sol
+++ fund.sol
@@ -19,10 +19,10 @@ contract FundMe{
       address [] public funder;
       
       function fund() public payable{                               //payment function
-          require (msg.value <= maxWei,"Dont fulfill the minimum");
+    require (msg.value>1e10,"Dont fulfill the minimum");
+       
           address _id=msg.sender;
-          owner=msg.sender;
-           adv[_id]+=msg.value*1e18;                               // store the address with value in adv mapping
+           adv[_id]+=msg.value;                               // store the address with value in adv mapping
            uint flag=0;
            for(uint8 i=0; i<funder.length; i++){                  //will loop through all and check whether funder is already present inside
             if(funder[i]==msg.sender) flag=1;                      //funders array if not then add
@@ -30,7 +30,7 @@ contract FundMe{
            if(flag==0) funder.push(msg.sender);
       }
      
-      function withdraw() public owne{                                //reverting all the transact till performed
+      function withdraw() public owne {                                //reverting all the transact till performed
         for(uint256 i=0; i<funder.length; i=i+1){
           address fundername=funder[i];
           adv[fundername]=0;
@@ -38,5 +38,9 @@ contract FundMe{
 
         funder=new address[](0);
             payable(msg.sender).transfer(address(this).balance);
+      }
+
+      function owntranfer(address _owner) public owne{        //only oner can tranfer ownership
+           owner=_owner;
       }
 }
